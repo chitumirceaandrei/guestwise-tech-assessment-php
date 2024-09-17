8d00f9f6e44fcf32b499bf5bddc2e8655b1fa949
MirceaA <chitumirceaandrei@gmail.com>
2024-09-17 13:36:46 +0300

    feat: add proper indexes to metrics tables for fetching table data faster

---
+++ database/migrations/2024_09_16_215613_add_index_to_conversions_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('conversions', function (Blueprint $table) {
+            $table->index('occurred_at', 'occurred_at_index');
+            $table->index(['brand_id', 'campaign_id'], 'brand_campaign_index');
+            $table->index(['campaign_id', 'occured_at'], 'campaign_occured_at_index');
+        });
+    }
+};
